741b350a6cd3bee81c1e8d578a962f27012f79f9
Cannigia Laluw <cannigialaluw@gmail.com>
2021-09-12 15:34:20 -0400

    Creating Exports for Stream.io API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+//public Stream.io api key
+const apikey = "9p8c89jewk2h";
+
+const client = StreamChat.getInstance(apikey);
 
 function App() {
   return (
-    <div>
-      <h1>High End Chat App!</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as ChannelContainer } from "./channel-container";
+export { default as ChannelListContainer } from "./channel-list-container";
